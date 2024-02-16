40082f6d829ee6b568cbc35d066f74d0c6e1e93f
Earyl James Buque <earyljames.buque@g.msuiit.edu.ph>
2024-02-16 13:23:01 +0800

    Map Update | Dalipuga-Kalubihon

---
+++ assets/js/north-baranggay.js
@@ -0,0 +1,110 @@
+var baranggayData = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "id": "01",
+        "properties": {
+          "name": "Dalipuga",
+          "density": 94.65
+        },
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [
+            [
+              [8.322986, 124.246573],
+              [8.320077, 124.259316],
+              [8.316186, 124.265528], // border misamis
+              [8.316181, 124.277298], // border misamis
+              [8.314578, 124.279212], // border bunawan
+              [8.311374, 124.277434], // border bunawan
+              [8.311374, 124.277434], // border kalubihon
+              [8.305488, 124.267724], // border kalubihon
+              [8.303171, 124.259586], // border kalubihon
+              [8.302051, 124.256782], // border kalubihon
+              [8.302698, 124.256052], // border kalubihon
+              [8.302937, 124.255404], // border kalubihon
+              [8.303043, 124.254294], // border sea
+              [8.303744, 124.254487], // border sea
+              [8.304731, 124.254482], // border sea
+              [8.305480, 124.254179], // border sea
+              [8.307075, 124.253116], // border sea
+              [8.308736, 124.251500], // border sea
+              [8.315119, 124.250697] // border sea
+            ]
+          ]
+        }
+      },
+
+      {
+        "type": "Feature",
+        "id": "72",
+        "properties": {
+          "name": "Kalubihon",
+          "density": 1082
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [8.311374, 124.277434], // border dalipuga
+                    [8.305488, 124.267724], // border dalipuga
+                    [8.303171, 124.259586], // border dalipuga
+                    [8.302051, 124.256782], // border dalipuga
+                    [8.302698, 124.256052], // border dalipuga
+                    [8.302937, 124.255404], // border dalipuga
+                    [8.302871, 124.254200], // border sea
+                    [8.301929, 124.254270], // border sea
+                    [8.298446, 124.254362], // border sea
+                    [8.299248, 124.255241], // border something
+                    [8.298438, 124.261290], // border something
+                    [8.299896, 124.265637], // border something
+                    [8.301093, 124.267876], // border something
+                    [8.300195, 124.272036], // border something falls
+                    [8.300132, 124.273687], // border something school start
+                    [8.299880, 124.274592], // border something
+                    [8.299444, 124.274506], // border something
+                    [8.299174, 124.275675], // border kiwalan
+                    [8.300830, 124.276226], // border kiwalan-bunawan
+                    [8.300832, 124.279633], // border kiwalan-bunawan
+                    [8.305039, 124.280995], // border bunawan
+                    [8.307460, 124.280904], // border bunawan
+                    [8.309145, 124.279100], // border bunawan
+                    [8.309145, 124.279100], // border bunawan
+                    [8.309742, 124.279659], // border bunawan
+                    [8.310177, 124.279686], // border bunawan
+                    [8.310119, 124.279091], // border bunawan
+                    [8.309822, 124.278641], // border bunawan
+                ]
+            ]
+        }
+    },      
+      {
+        "type": "Feature",
+        "id": "72",
+        "properties": {
+          "name": "Puerto Rico",
+          "density": 1082
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [-66.448338,17.984326], 
+                    [-66.771478,18.006234],
+                    [-66.924832,17.929556],
+                    [-66.985078,17.973372],
+                    [-67.209633,17.956941],
+                    [-67.154863,18.19245],
+                    [-67.269879,18.362235],
+                    [-65.626797,18.203403],
+                    [-65.730859,18.186973],
+                    [-66.234737,17.929556],
+                    [-66.448338,17.984326]
+                ]
+            ]
+        }
+    }
+            
+]
+};
